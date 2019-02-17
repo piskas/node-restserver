d2789086f9daf0631defb8e2a1747876e67adb6f
Luis Arellano Vasquez <luis.arellanovz@gmail.com>
2019-02-17 20:43:00 -0300

    Commit inicial

---
+++ server/server.js
@@ -0,0 +1,47 @@
+// REQUIRE'S
+require('./config/config')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+// CONFIG Body-Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// SERVICIOS DE USUARIO
+app.get('/usuario', function(req, res) {
+    res.json('get usuario');
+});
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    } else {
+        res.json({
+            persona: body
+        });
+    }
+});
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+});
+app.delete('/usuario', function(req, res) {
+    res.json('delete usuario');
+});
+
+
+
+// LISTENER
+app.listen(process.env.PORT, () => {
+    console.log("Escuchando el puerto", process.env.PORT);
+});
